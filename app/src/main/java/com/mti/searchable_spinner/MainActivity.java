d3fb87f2b8f9c49fdab923ae9b2d832f2950a460
Tareq3 <mti.tareq3@gmail.com>
2018-09-11 19:12:11 +0600

    v1.4

--- app/src/main/java/com/mti/searchable_spinner/MainActivity.java
+++ app/src/main/java/com/mti/searchable_spinner/MainActivity.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> mArrayList=new ArrayList<>(Arrays.asList("Red_Apple","Green_Apple","Yellow_Banana" ,"orange", "Goava", "potato","Beans","cucamber","carrot","apple_syrup"  ));
+    ArrayList<String> mArrayList=new ArrayList<>(Arrays.asList("Red_Apple","Green_Apple","Yellow_Banana" ,"orange", "Goava", "potato","Beans","cucamber","carrot","apple_syrup","Red_Apple","Green_Apple","Yellow_Banana" ,"orange", "Goava", "potato","Beans","cucamber","carrot"  ));
 
     SearchableSpinner    searchableSpinner;
     @Override
